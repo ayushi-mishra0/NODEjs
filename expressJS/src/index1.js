ff31a3b0f8168539692368ebd1e4ed15820b947a
Ayushi <msseema007@gmail.com>
2024-08-01 10:01:23 +0530

    expressJS

--- expressJS/src/index1.js
+++ expressJS/src/index1.js
@@ -1,15 +1,30 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const hbs = require("hbs");
 const port = 3000;
 
-//buildin  middleware
-const staticPath = path.join(__dirname,"../public");
+// Build the path for static files
+const staticPath = path.join(__dirname, "../public");
+const templatePath = path.join(__dirname,"../template");
+const partialsPath = path.join(__dirname,"../templates/partials");
+
+// Set the view engine to hbs
+app.set("view engine", "hbs");
+app.set('views',templatePath);
+hbs.registerPartials(partialsPath);
+// Set the views directory (if necessary)
+// app.set("views", path.join(__dirname, "../views"));
+ 
+// Serve static files from the public directory
 app.use(express.static(staticPath));
-app.get("/",(req,res)=>{
-    res.send("hello")
+
+// Route to render the index view
+app.get("/", (req, res) => {
+    res.render('index');
 });
 
-app.listen(port, () =>{
-    console.log(`listening to the port ${port}`);
+// Start the server
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
 });
